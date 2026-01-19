fd86469cb74acdf376dca2b6dcd40cb782f6b1e5
Mubarak <ymubarak872@gmail.com>
2026-01-19 11:23:27 +0100

    first commit

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ alert ("Thank you for your interest. I will reach out soon!");
 
 }) ;
 // Contact Form Validation
-document. getElementById ("contactForm"). addEventListener ("submit", function(e){
-e. preventDefault ();
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-let name = document.getElementById ("contactName"). Value;
-let email = document.getElementById ("contactEmail"). Value;
-let msg = document.getElementById ("message");
-let feedback = document-getElementById ("feedback" ) ;
+    let name = document.getElementById("contactName").value;
+    let email = document.getElementById("contactEmail").value;
+    let msg = document.getElementById("message").value;
+    let feedback = document.getElementById("feedback");
 
-if (name ==="" || email === "" || msg === ""){
-feedback. textContent = "All fields are required!";
-feedback. style.color = "red";
-return;
-}
+    if (name === "" || email === "" || msg === "") {
+        feedback.textContent = "All fields are required!";
+        feedback.style.color = "red";
+        return;
+    }
 
-feedback.textContent = "Message sent sucessfully!";
+    feedback.textContent = "Message sent successfully!";
+    feedback.style.color = "green";
 });
